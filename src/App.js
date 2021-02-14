f3170a65c733f1db29b1a2c06151f2da158b7272
Amy-glitch <ccheetah3012@gmail.com>
2021-02-15 00:20:13 +0200

    1st changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Nav from './Nav.js';
+import Login from './Login.js';
+import {DbProvider} from './Ctx.js';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div>
-     This is div
-    </div>
-  );
+  const [log,setLog] = useState(false);
+
+  if(log === true)
+  {
+
+  return <DbProvider ><Nav/></DbProvider>
+  
+  }
+  else
+  {
+  return   <DbProvider  > <Login setLog={setLog}></Login>  </DbProvider>
+  }
 }
 
 export default App;
